66e86803008792984507b25c64d78c0a8e8d5b7b
VIKAS PATEL <63722887+vikas6377297826@users.noreply.github.com>
2022-04-07 07:59:56 +0530

    creating contact components

--- huntingcoders/pages/contact.js
+++ huntingcoders/pages/contact.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Contact.module.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <h1 className={styles.heading}>Contact</h1>
+        <p style={{ textAlign: "center" }}>
+          If you have any questions, please feel free to contact me.
+        </p>
+        <div className={styles.contacForm}>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.formGroup}>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" rows="6"></textarea>
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Contact;
